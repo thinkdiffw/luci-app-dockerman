0303e2a4a320b090a06b74f196c37a9d63cd3289
thinkdiffw <thinkdiffw@gmail.com>
2022-12-22 16:51:15 +0800

    change submenu order

--- applications/luci-app-dockerman/luasrc/controller/dockerman.lua
+++ applications/luci-app-dockerman/luasrc/controller/dockerman.lua
@@ -14,7 +14,7 @@ function index()
 		_("Docker"),
 		40).acl_depends = { "luci-app-dockerman" }
 
-	entry({"admin", "docker", "config"},cbi("dockerman/configuration"),_("Configuration"), 8).leaf=true
+	entry({"admin", "docker", "config"},cbi("dockerman/configuration"),_("Configuration"), 1).leaf=true
 	
 	-- local uci = (require "luci.model.uci").cursor()
 	-- if uci:get_bool("dockerd", "dockerman", "remote_endpoint") then
